53a6ed88e78c79b710ce3d8b82d6089d6008003f
Sue Ann <sueannprewitt@yahoo.com>
2017-09-07 15:44:32 -0400

    Initial commit

--- StudentClassProject/Program.cs
+++ StudentClassProject/Program.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace StudentClassProject {
 	class Program {
+
+		void Run () {
+			Student sa = new Student("Sue Ann", "Hazel");
+			Student eb = new Student("Eric B", "Brown");
+			Student go = new Student("Greg", "Blue");
+			Student be = new Student("Bob", "Blue");
+			Student es = new Student("Eric S", "Blue");
+			}
+
+
+
 		static void Main(string[] args) {
+			new Program().Run();
 		}
 	}
 }
---
+++ StudentClassProject/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentClassProject {
+	class Student {
+		//Properties
+		string Name;
+		string EyeColor;
+		DateTime DateRegistered = DateTime.Now;
+		bool LikesMusic = true; //default value
+		int YearsExperience = 1;
+		
+		
+		
+		//Methods
+		public void SetDateRegistered(DateTime Registered) {
+			 DateRegistered= Registered;
+		}
+		public void SetName(string name) {
+			Name = name;
+		}
+		public void SetEyeColor(string color = "Brown") {
+			EyeColor = color;
+		}
+		public void SetLikesMusic (bool likesMusic) {
+			LikesMusic = likesMusic;
+		}
+		public void SetYearsExperience(int years) {
+			YearsExperience = years;
+		}
+		//constructor (special method)
+		//default constructor
+		public Student() {
+
+		}
+
+		public Student (string name, string eyeColor) {
+			Name = name;
+			EyeColor = eyeColor;
+		}
+		}
+}
